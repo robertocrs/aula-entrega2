2f6eb9732c173548a1cfa153ff2c153da43a1c71
robertocrs <107523307+robertocrs@users.noreply.github.com>
2022-06-16 22:25:28 -0300

    Add files via upload

---
+++ Moto.java
@@ -0,0 +1,27 @@
+
+public class Moto {
+
+
+	String modelo;
+	int qtde;
+
+
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public int getQtde() {
+		return qtde;
+	}
+	public void setQtde(int qtde) {
+		this.qtde = qtde;
+	}
+	public static void incluirMoto(){
+		System.out.println("Você entrou no método Inclui Motocicleta.");
+	}
+}	
+
+
+
---
+++ Venda.java
@@ -0,0 +1,11 @@
+
+public class Venda {
+	
+	String data, nome, modelo;
+	public static void venderMoto(){
+		System.out.println("Você entrou no método Vender Motoclicleta.");
+	}
+	public static void listarVendas(){
+		System.out.println("Você entrou no método Listar Vendas de Motoclicleta.");
+	}	
+}
